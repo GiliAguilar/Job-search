79a9d0fca14c6f0cc1c0b753a4054a555cb391e9
GiliAguilar <114941917+GiliAguilar@users.noreply.github.com>
2023-08-16 00:18:42 +0200

    inicio de migración de Vue 2 a Vue 3.2

--- src/components/Shared/ActionButton.vue
+++ src/components/Shared/ActionButton.vue
@@ -10,7 +10,10 @@
   </button>
 </template>
 
-<script>
+<!-- Antigua forma de escribir en Vue2 y Vue3, mixto -->
+<!-- <script>
+import { computed, toRefs } from 'vue';
+
 export default {
   name: 'ActionButton',
   props: {
@@ -30,29 +33,74 @@ export default {
       //al poner require, le estamos diciendo si es obligatorio que nos especifiquen el dato en el componente padre (MainNave -> action-button type="primary"), en caso de ser falso, con default le especificamos el contenido que tendrá por defecto si NO SE APLICA NADA, si se aplica algo que no es se maneja con validator
     },
   },
-
   //props es un pedazo de código (argumentos) que pasa de su componente padre (parent) a sus componentes hijos (child), osea, en este ejemplo, de MainNav.vue a ActionButton.vue. la ventaja de esto es que cualquier cambio que sufra un argumento de componente padre, va a auto renderizar en el momento lo afectado del componente hijo
-  computed: {
-    buttonClass() {
-      return {
-        [this.type]: true,
-        /* 
-        primary: this.type === 'primary',
-        secondary: this.type === 'secondary',
-        podemos escribirlo de esta forma 
-        
-        o usar javascript Dinamic Object Keys, se pone entre "[]" el nombre de la propiedad del objeto, antes, externamente creamos una variable la cual buscará y asignará como nombre de la propiedad del objeto, ejem:
+  //Vue 3
+  setup(props) {
+    //si necesitamos usar props simplemente las pasamos como un parámetro en la función setup
 
-        const favoriteFood = "sushi";
-        const goodFoods = {
-          [favoriteFood]: true,
-        };
-        console.log(goodFoods) -> //{ sushi: true }
-         */
-      };
-    },
+    //metodo 1
+    // const buttonClass = computed(() => {
+    //   return { [props.type]: true };
+    // });
+
+    //metodo 2
+    const { type } = toRefs(props);
+    //toma el props que está recibiento este componente Vue, suponiendo tiene nombre "Hola", y props devuelve el contenido del prop, osea devuelve el contenido de "Hola", que sería {type, requiere, etc...}, luego destructuramos y extraemos solo type, que lo devuelve como un nuevo objeto, el cual usaremos en la función buttonClass; como type realmente es un valor de prop, debemos referenciar a esta siempre con [type] y objetener su contenido [type.value]
+    const buttonClass = computed(() => {
+      return { [type.value]: true };
+    });
+    return { buttonClass };
   },
+
+  //Vue 2
+  // computed: {
+  //   buttonClass() {
+  //     return {
+  //       [this.type]: true,
+  //       /*
+  //       primary: this.type === 'primary',
+  //       secondary: this.type === 'secondary',
+  //       podemos escribirlo de esta forma
+
+  //       o usar javascript Dinamic Object Keys, se pone entre "[]" el nombre de la propiedad del objeto, antes, externamente creamos una variable la cual buscará y asignará como nombre de la propiedad del objeto, ejem:
+
+  //       const favoriteFood = "sushi";
+  //       const goodFoods = {
+  //         [favoriteFood]: true,
+  //       };
+  //       console.log(goodFoods) -> //{ sushi: true }
+  //        */
+  //     };
+  //   },
+  // },
 };
+</script> -->
+
+<!-- nueva forma con Vue 3.2 -->
+<script setup>
+import { computed, toRefs } from 'vue';
+
+//no necesitamos usar export default, ni utilizar return, ni .this, tampoco data, methods, computed, etc..., todo lo que esté aquí definido estará disponible para <template> y <style>
+const props = defineProps({
+  text: {
+    type: String,
+    require: true,
+    default: null,
+  },
+  type: {
+    type: String,
+    require: false,
+    validator(value) {
+      return ['primary', 'secondary'].includes(value.toLowerCase());
+    },
+    default: 'primary',
+  },
+});
+
+const { type } = toRefs(props);
+const buttonClass = computed(() => {
+  return { [type.value]: true };
+});
 </script>
 
 <style scoped>
--- src/components/Shared/CollapsibleAccordion.vue
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -18,33 +18,77 @@
   </div>
 </template>
 
-<script>
+<!-- Antigua forma de escribir en Vue 2 y Vue 3 -->
+<!-- <script>
+//Vue 3
+import { ref, computed } from 'vue';
+
 export default {
   name: 'CollapsibleAccordion',
-
+  //Vue 3
+  //Vue 2 utiliza básicamente los componentes que lo conforman por separado en el script, osea, props, data, computed, methods, etc, con Vue 3 utilizamos un solo archivo de configuración para todo, llamado setup() {}, el cual siempre debe regresar la información que será utiliada por la sección de template y contiene básicamente todo lo que contiene Vue2 pero en un entorno identico a JavaScript, sin embargo, props y algunas otras funciones siempre deben mantenerse fuera de setup
   props: {
     header: {
       type: String,
       required: true,
     },
   },
+  setup() {
+    const isOpen = ref(false);
 
-  data() {
-    return {
-      isOpen: false,
+    const open = () => {
+      isOpen.value = !isOpen.value;
     };
+    const caretIcon = computed(() => (isOpen.value ? 'fa-angle-up' : 'fa-angle-down'));
+
+    return { isOpen, open, caretIcon };
+    //en return, realmente estamos devolviendo objeto header, sin embargo, Vue reconoce que es un objeto reactive, por lo que no requiere que en <template> necesitemos tuilizar header.value, automáticamente reconoce el valor del objeto, por lo que podemos llamar directamente como header en template
   },
 
-  computed: {
-    caretIcon() {
-      return this.isOpen ? 'fa-angle-up' : 'fa-angle-down';
-    },
-  },
+  //Vue 2, optional method
+  // props: {
+  //   header: {
+  //     type: String,
+  //     required: true,
+  //   },
+  // },
 
-  methods: {
-    open() {
-      this.isOpen = !this.isOpen;
-    },
-  },
+  // data() {
+  //   return {
+  //     isOpen: false,
+  //   };
+  // },
+
+  // computed: {
+  //   caretIcon() {
+  //     return this.isOpen ? 'fa-angle-up' : 'fa-angle-down';
+  //   },
+  // },
+
+  // methods: {
+  //   open() {
+  //     this.isOpen = !this.isOpen;
+  //   },
+  // },
 };
+</script> -->
+
+<!-- Nueva forma de escribir con Vue 3.2 -->
+<script setup>
+import { computed, ref } from 'vue';
+
+defineProps({
+  header: {
+    type: String,
+    required: true,
+  },
+});
+
+const isOpen = ref(false);
+
+const open = () => {
+  isOpen.value = !isOpen.value;
+};
+
+const caretIcon = computed(() => (isOpen.value ? 'fa-angle-up' : 'fa-angle-down'));
 </script>
